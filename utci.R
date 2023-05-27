339f2e31ea4472d9fe2cc2707700f4f6d0c3eee1
ryan scharf <ryan.scharf@gmail.com>
2023-05-26 20:34:07 -0400

    we did it!

--- utci.R
+++ utci.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(sf)
 library(stars)
 library(tmap)
+library(patchwork)
 
 st <- tigris::states(cb=T)
 
@@ -16,34 +17,34 @@ categories <- data.frame(
               high = c(999L, 45L, 37L, 31L, 25L, 8L, -1L, -14L, -27L, -40L)
 )
 
-
-nc <- read_ncdf('ECMWF_utci_20191001_v1.1_int.nc')
-nc_c <- (nc - 273.15)
-
-nc_c <- st_crop(nc_c, 
-        st_bbox(
-          c(
-            xmin = -124.848974, 
-            xmax = -66.885444, 
-            ymax = 49.384358, 
-            ymin = 24.396308),
-          crs = st_crs(4326)
-          )
-        )
-
-nc_c %>% 
-  #slice('time',1) %>% 
-  tm_shape() + 
-    tm_raster(
-              breaks = c(-273, -40, -27, -13, 0, 9, 26, 32, 38, 46, 100),
-              labels = categories$category,
-              midpoint = 0
-              ) + 
-  tm_shape(st) + 
-    tm_borders()
-
-maxx <- nc_c %>% aggregate(by = '1 day', max) 
-minn <- nc_c %>% aggregate(by = '1 day', min) 
+# 
+# nc <- read_ncdf('ECMWF_utci_20191001_v1.1_int.nc')
+# nc_c <- (nc - 273.15)
+# 
+# nc_c <- st_crop(nc_c, 
+#         st_bbox(
+#           c(
+#             xmin = -124.848974, 
+#             xmax = -66.885444, 
+#             ymax = 49.384358, 
+#             ymin = 24.396308),
+#           crs = st_crs(4326)
+#           )
+#         )
+# 
+# nc_c %>% 
+#   #slice('time',1) %>% 
+  # tm_shape() +
+  #   tm_raster(
+  #             breaks = c(-273, -40, -27, -13, 0, 9, 26, 32, 38, 46, 100),
+  #             labels = categories$category,
+  #             midpoint = 0
+  #             ) +
+  # tm_shape(st) +
+  #   tm_borders()
+# 
+# maxx <- nc_c %>% aggregate(by = '1 day', max) 
+# minn <- nc_c %>% aggregate(by = '1 day', min) 
 
 good_index <- function(x){
   #  sum(
@@ -58,20 +59,24 @@ good_index <- function(x){
 # 
 # gi2 %>% aggregate(by = '1 day', sum) %>% tm_shape() + tm_raster() + tm_shape(st) + tm_borders()
 
-path <- 'C:/Users/ryans/Downloads/dataset-derived-utci-historical.zip'
+path <- paste0(here::here(),'/data/dataset-derived-utci-historical.zip')
 files <- unzip(path, list = T)
+files <- files %>% arrange(Name)
 
 #drop first and last files. they're incomplete
-files <- files[c(-1, -nrow(files)),]
+files <- files[c(-1, -nrow(files)), ]
 
-agg_gi <- function(fl, path = 'C:/Users/ryans/Downloads/dataset-derived-utci-historical.zip',
-         output_file = 'utci_daily_good_index.rds'){
+agg_gi <- function(fl, 
+         path = paste0(here::here(),'/data/dataset-derived-utci-historical.zip'),
+         output_file = paste0(here::here(),'utci_daily_good_index.rds')){
+  
+  cat(paste0('\n', fl, '\n'))
   
   dir <- tempdir()
   
   file <- unzip(path, file = fl, exdir = dir)
   
-  nc <- read_ncdf(paste(dir, fl, sep = '/'))
+  nc <- read_ncdf(paste(dir, fl, sep = '/'), var = 'utci')
 
   #convert K to C
   nc_c <- (nc - 273.15)
@@ -91,7 +96,7 @@ agg_gi <- function(fl, path = 'C:/Users/ryans/Downloads/dataset-derived-utci-his
                   )
   
   gi <- nc_c %>% aggregate(by = '1 hour', FUN = good_index)
-  gi2 <- gi %>% aggregate(by = '1 day', sum)
+  gi2 <- gi %>% aggregate(by = '1 day', sum, na.rm = T)
   
   gi2 <- aperm(gi2, c(2,3,1))
   gi2 <- st_set_dimensions(gi2, 3, 
@@ -108,6 +113,54 @@ agg_gi <- function(fl, path = 'C:/Users/ryans/Downloads/dataset-derived-utci-his
   }
 }
 
-purrr::walk(files$Name, agg_gi)
+purrr::walk(files$Name, agg_gi, .progress = T)
 
 readRDS('utci_daily_good_index.rds')
+
+breaks = c(3.5,4,4.5,5,5.5,6,6.5,7,7.5,8)
+
+p1 <- readRDS('utci_daily_good_index.rds') %>% 
+  aggregate(by = '8 years', FUN = mean, na.rm = T) %>% 
+  aggregate(by = counties, min, na.rm = T) %>% st_as_sf() %>%
+  rename(dz =`2019-01-01`) %>% 
+  ggplot() + 
+  geom_sf(aes(fill = dz), color = 'black') +
+  scale_fill_viridis_b(breaks = c(3.5,4,4.5,5,5.5,6,6.5,7,7.5,8),  option ='H') +
+  ggthemes::theme_few() + 
+  ggtitle('Average Hours of Optimal Comfort Index (County-level minimum)',
+          subtitle = 'UTCI from ERA-5 analysis, 10/01/2019-05/13/2023')
+  
+p2 <- readRDS('utci_daily_good_index.rds') %>% 
+  aggregate(by = '8 years', FUN = mean, na.rm = T) %>% 
+  aggregate(by = counties, max, na.rm = T) %>% st_as_sf() %>%
+  rename(dz =`2019-01-01`) %>% 
+  ggplot() + 
+  geom_sf(aes(fill = dz), color = 'black') +
+  scale_fill_viridis_b(breaks = c(3.5,4,4.5,5,5.5,6,6.5,7,7.5,8), option ='H') +
+  ggthemes::theme_few() +
+  ggtitle('Average Hours of Optimal Comfort Index (County-level maximum)',
+          subtitle = 'UTCI from ERA-5 analysis, 10/01/2019-05/13/2023')
+
+p3 <- readRDS('utci_daily_good_index.rds') %>% 
+  aggregate(by = '8 years', FUN = mean, na.rm = T) %>% 
+  aggregate(by = counties, mean, na.rm = T) %>% st_as_sf() %>%
+  rename(dz =`2019-01-01`) %>% 
+  ggplot() + 
+  geom_sf(aes(fill = dz), color = 'black') +
+  scale_fill_viridis_b(breaks = c(3.5,4,4.5,5,5.5,6,6.5,7,7.5,8), option ='H') +
+  ggthemes::theme_few() +
+  ggtitle('Average Hours of Optimal Comfort Index (County-level mean)',
+          subtitle = 'UTCI from ERA-5 analysis, 10/01/2019-05/13/2023')
+
+p4 <- readRDS('utci_daily_good_index.rds') %>% 
+  aggregate(by = '8 years', FUN = median, na.rm = T) %>% 
+  aggregate(by = counties, median, na.rm = T) %>% st_as_sf() %>%
+  rename(dz =`2019-01-01`) %>% 
+  ggplot() + 
+  geom_sf(aes(fill = dz), color = 'black') +
+  scale_fill_viridis_b(breaks = c(3.5,4,4.5,5,5.5,6,6.5,7,7.5,8), option ='H') +
+  ggthemes::theme_few() +
+  ggtitle('Average Hours of Optimal Comfort Index (County-level median)',
+          subtitle = 'UTCI from ERA-5 analysis, 10/01/2019-05/13/2023')
+
+p1/p2/p3/p4
